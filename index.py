cb6329076d4f73360af916ec683125020f6d8b0b
Sadik0000 <sadikahmedtanjim@gmail.com>
2025-07-21 18:05:45 +0600

    print() method-added

--- index.py
+++ index.py
@@ -1,1 +1,19 @@
 print("Hello world!")
+
+print("Python – Versatile and beginner-friendly, widely used in AI, data science, and web development.
+
+Java – A staple in enterprise applications and Android development.
+
+JavaScript – Essential for web development, powering dynamic web pages.
+
+C++ – Preferred for high-performance applications, such as game engines and real-time systems.
+
+C# – Commonly used in Windows applications and game development with Unity.
+
+TypeScript – A superset of JavaScript that adds static typing, enhancing code quality.
+
+SQL – Crucial for database management and data analysis.
+
+C – Foundational language, still relevant in system programming and embedded systems.
+
+Go (Golang) – Known for its simplicity and efficiency in backend development.")
